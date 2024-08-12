d59088762489f13e8e81fdad471bfb1e48abde41
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-08-12 17:53:21 -0500

    add page reservations for create customers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Customer from './components/pages/customer'
 import Footer from './layout/footer'
 import Navbar from './layout/navbar'
+import Customers from './components/pages/Customers'
 
 export default function App() {
 
   return (
     <div className="container">
-    <Navbar/>
-    <Customer/>
-    <Footer/>
+      <Navbar/>
+      <Customers/>
+      <Footer/>
     </div>
   )
 }
