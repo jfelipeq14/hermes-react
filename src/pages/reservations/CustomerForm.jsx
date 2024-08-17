7797c32ef6022fb30cf2a83e528d6519d7438aba
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-08-17 09:39:24 -0500

    add validation in customer form: no review

--- src/pages/reservations/CustomerForm.jsx
+++ src/pages/reservations/CustomerForm.jsx
@@ -1,149 +1,223 @@
-import { Customer } from "../../models/customer";
+import { useState } from "react";
+import { Customer } from "../../models/customer.model";
 import { documentTypes } from "../../utilies/documentTypes";
 import { phonePrefixes } from "../../utilies/phonePrefixes";
+import { User } from "../../models/user.model";
 
 // eslint-disable-next-line react/prop-types
 export default function CustomerForm() {
-  const customer = new Customer();
+  const formCustomer = new Customer();
+  const formUser = new User();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(customer);
+  const [customer, setCustomer] = useState(formCustomer);
+  const [user, setUser] = useState(formUser);
+  const [error, setError] = useState(false);
+
+  const handleChangeCustomer = (e) => {
+    const { name, value, checked, type } = e.target;
+    setCustomer({ ...customer, [name]: type === "checkbox" ? checked : value });
   };
 
+  const handleChangeUser = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // pequeña validación
+    if (!customer.documentType.trim() || !customer.identification.trim()) {
+      console.log("campos vacíos");
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+
+    // Enviar todo a un array!
+  };
+
+  const PintarError = () => (
+    <div className="alert alert-danger">Todos los campos obligatorios</div>
+  );
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group my-2">
-        <label htmlFor="identification" className="col-12">
-          Cedula:
-        </label>
-        <div className="d-flex justify-content-around">
-          <select className="form-select" aria-label="Default select example">
-            <option selected>Seleccione</option>
-            {documentTypes.map((documentType) => (
-              <option key={documentType}>{documentType}</option>
-            ))}
-          </select>
-          <input type="text" className="form-control" />
-          <button className="btn btn-primary">🔍</button>
-        </div>
-      </div>
-      <div className="form-group my-2">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="firstName">Nombre nombre:</label>
+    <div className="container">
+      {error && <PintarError />}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group my-2">
+          <label htmlFor="identification" className="col-12">
+            Cedula:
+          </label>
+          <div className="d-flex justify-content-around">
+            <select
+              className="form-select"
+              name="documentType"
+              value={customer.documentType}
+              onChange={handleChangeCustomer}
+            >
+              <option selected>Seleccione</option>
+              {documentTypes.map((documentType) => (
+                <option key={documentType}>{documentType}</option>
+              ))}
+            </select>
             <input
               type="text"
               className="form-control"
-              value={customer.name}
-              onChange={(event) => {
-                customer.name = event.target.value;
-              }}
+              value={customer.identification}
+              onChange={handleChangeCustomer}
             />
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="secondName">Segundo nombre:</label>
-            <input type="text" className="form-control" id="secondName" />
+            <button className="btn btn-primary">🔍</button>
           </div>
         </div>
-      </div>
-      <div className="form-group my-2">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="firstLastName">Primer apellido:</label>
-            <input type="text" className="form-control" id="firstLastName" />
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="secondLastName">Segundo apellido:</label>
-            <input type="text" className="form-control" id="secondLastName" />
-          </div>
-        </div>
-      </div>
-      <div className="form-group my-2">
-        <label htmlFor="identification" className="col-12">
-          Contacto:
-        </label>
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <select className="form-select" aria-label="Default select example">
-              <option selected>Seleccione</option>
-              {phonePrefixes.map((phonePrefix) => (
-                <option
-                  key={phonePrefix.prefix}
-                >{`${phonePrefix.country} (${phonePrefix.prefix})`}</option>
-              ))}
-            </select>
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <input type="text" className="form-control" />
+        <div className="form-group my-2">
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="name">Nombres:</label>
+              <input
+                type="text"
+                className="form-control"
+                value={customer.name}
+                onChange={handleChangeCustomer}
+              />
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="lastName">Apellidos:</label>
+              <input
+                type="text"
+                className="form-control"
+                value={customer.lastName}
+                onChange={handleChangeCustomer}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-group my-2">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="birthDate">Fecha de nacimiento:</label>
-            <input type="date" className="form-control" id="birthDate" />
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="age">Edad:</label>
-            <input type="number" className="form-control" id="age" readOnly />
-          </div>
-        </div>
-      </div>
-      <div className="form-group my-2">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="email">Correo:</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="emailConfirmation">Confirmación de correo:</label>
-            <input
-              type="email"
-              className="form-control"
-              id="emailConfirmation"
-            />
+        <div className="form-group my-2">
+          <label htmlFor="phone" className="col-12">
+            Celular:
+          </label>
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <select className="form-select">
+                <option selected>Seleccione</option>
+                {phonePrefixes.map((phonePrefix) => (
+                  <option
+                    key={phonePrefix.prefix}
+                  >{`${phonePrefix.country} (${phonePrefix.prefix})`}</option>
+                ))}
+              </select>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <input
+                type="text"
+                className="form-control"
+                value={customer.phone}
+                onChange={handleChangeCustomer}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-group my-2">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="password">Contraseña:</label>
-            <input type="password" className="form-control" id="password" />
-          </div>
-          <div className="col-sm-12 col-md-6">
-            <label htmlFor="passwordConfirmation">
-              Confirmación de contraseña:
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="passwordConfirmation"
-            />
+        <div className="form-group my-2">
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="birthDate">Fecha de nacimiento:</label>
+              <input
+                type="date"
+                className="form-control"
+                value={customer.dateOfBirth}
+                onChange={handleChangeCustomer}
+              />
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="age">Edad:</label>
+              <input
+                type="number"
+                className="form-control"
+                name="age"
+                value={customer.age}
+                readOnly
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-check my-2">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          value=""
-          id="travel"
-        />
-        <label className="form-check-label" htmlFor="travel">
-          El cliente está incluido en el viaje
-        </label>
-      </div>
-      <div className="buttons">
-        <button type="submit" className="btn btn-primary">
-          Crear
-        </button>
-        <button type="reset" className="btn btn-secondary">
-          Limpiar
-        </button>
-      </div>
-    </form>
+        <div className="form-group my-2">
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="email">Correo:</label>
+              <input
+                type="email"
+                className="form-control"
+                value={user.email}
+                onChange={handleChangeUser}
+              />
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="emailConfirmation">Confirmación de correo:</label>
+              <input
+                type="email"
+                className="form-control"
+                value={user.email}
+                onChange={(e) => {
+                  if (user.email === e.target.value) {
+                    return;
+                  }
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="form-group my-2">
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="password">Contraseña:</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                value={user.password}
+                onChange={handleChangeUser}
+              />
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <label htmlFor="passwordConfirmation">
+                Confirmación de contraseña:
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                name="passwordConfirmation"
+                value={user.password}
+                onChange={(e) => {
+                  if (user.password === e.target.value) {
+                    return;
+                  }
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="form-check my-2">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="travel"
+            value={false}
+            onChange={handleChangeCustomer}
+          />
+          <label className="form-check-label" htmlFor="travel">
+            El cliente está incluido en el viaje
+          </label>
+        </div>
+        <div className="buttons">
+          <button type="submit" className="btn btn-primary">
+            Crear
+          </button>
+          <button type="reset" className="btn btn-secondary">
+            Limpiar
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
