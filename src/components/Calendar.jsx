121485d92fdded13fd9b1ea777d1fff257d83ca4
Brayan0813 <brayansanchez0813@gmail.com>
2024-08-28 10:55:44 -0500

    Init Programing

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,23 +1,18 @@
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
-const events = [
-  { title: 'Meeting', start: new Date() }
-]
+const events = [{ title: "Meeting", start: new Date() }];
 
 export function Calendar() {
   return (
-    <div>
-      <h1>Demo App</h1>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView='dayGridMonth'
-        weekends={false}
-        events={events}
-        eventContent={renderEventContent}
-      />
-    </div>
-  )
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      weekends={false}
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
 }
 
 // a custom render function
@@ -27,5 +22,5 @@ function renderEventContent(eventInfo) {
       <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
     </>
-  )
+  );
 }
