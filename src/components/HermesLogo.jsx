2d9daa965260bc4bacad3627aad5fd4e42415eb5
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-09-06 12:39:53 -0500

    modify sidebar

--- src/components/HermesLogo.jsx
+++ src/components/HermesLogo.jsx
@@ -2,9 +2,9 @@ export default function HermesLogo() {
   return(
     // agregar logo de hermes ubicado en los assets
     <picture>
-      <source srcSet="../../public/hermes.svg" type="image/webp" />
-      <source srcSet="../../public/hermes.svg" type="image/png" />
-      <img src="../../public/hermes.svg" className="img-fluid" alt="Hermes" />
+      <source srcSet="/hermes.svg" type="image/webp" />
+      <source srcSet="/hermes.svg" type="image/png" />
+      <img src="/hermes.svg" className="img-fluid" alt="Hermes" />
     </picture>
   )
 }
